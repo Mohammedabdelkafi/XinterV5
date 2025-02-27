49745eae0c01abaf01623a1288a447dcad205228
Mohammed abdelkafi <kawruusir@gmail.com>
2025-02-27 22:45:19 +0100

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 char src[256];
+int advance();
 int pos;
+char CC[256];
 int main() {
 	printf("Xinter ===>");
 	scanf("%s", &src);
 	printf("%s", src);
 	return 0;
+}
+int advance() {
+	pos++;
+	char CC[] = src[pos];
 }
