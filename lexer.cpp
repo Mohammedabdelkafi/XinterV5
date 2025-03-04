bf3363af1f40e9ab18ceb73d5d026f24664a2838
Mohammed abdelkafi <kawruusir@gmail.com>
2025-03-05 00:15:05 +0100

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,20 +1,64 @@
 #include <stdio.h>
-char src[256];
-int advance();
+#include <stdbool.h>
+#include <string.h>
+
+char sourceCode[256];
+char letters[] = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+char digits[] = "12345678890";
+char BinOP[] = "+-/*";
+char curr_char;
 int pos = -1;
-char CC[256];
+bool string = false;
+bool integer = false;
+char tokens[256][256];
+int token_count = 0;
+
+void input() {
+    printf("Xinter > ");
+    scanf("%s", sourceCode);
+}
+
+void advance() {
+    pos++;
+    curr_char = sourceCode[pos];
+}
+
+void append_token(const char* token) {
+    strcpy(tokens[token_count], token);
+    token_count++;
+}
+
+void tokenize() {
+    while (sourceCode[pos] != '\0') {
+        advance();
+        switch (curr_char) {
+            case '+':
+                append_token("PLUS");
+                break;
+            case '-':
+                append_token("MINUS");
+                break;
+            case '/':
+                append_token("DIV");
+                break;
+            case '*':
+                append_token("MUL");
+                break;
+            default:
+                // Handle other characters or skip them
+                break;
+        }
+    }
+}
+
 int main() {
-	printf("Xinter ===>");
-	scanf("%s", &src);
-	printf("%s", src);
-	return 0;
-}
-int advance() {
-	pos++;
-	char CC[] = src[pos];
-}
-int tokenize() {
-	if (CC=="+") {
-		printf("yes :)");
-	}
+    input();
+    tokenize();
+    
+    // Print tokens for verification
+    for (int i = 0; i < token_count; i++) {
+        printf("Token: %s\n", tokens[i]);
+    }
+
+    return 0;
 }
